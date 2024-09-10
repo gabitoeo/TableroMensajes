33a32b4435f02c9f463271991e7b8b3a9301e9f8
gabitoeo <gebelen@udc.edu.ar>
2024-09-10 15:58:42 -0300

    changes

--- TableroMensajes/mensajes/views.py
+++ TableroMensajes/mensajes/views.py
@@ -19,31 +19,25 @@ def crear_mensaje(request):
 
 
 def ver_mensajes_recibidos(request):
-    # Usar el usuario predeterminado para esta vista
     usuario_predeterminado = User.objects.get(username='predeterminado')
     mensajes = Mensaje.objects.filter(destinatario=usuario_predeterminado)
     return render(request, 'mensajes/mensajes_recibidos.html', {'mensajes': mensajes})
 
 def ver_mensajes_enviados(request):
-    # Siempre usar el usuario predeterminado para esta vista
     usuario_predeterminado = User.objects.get(username='predeterminado')
     mensajes = Mensaje.objects.filter(remitente=usuario_predeterminado)
     return render(request, 'mensajes/mensajes_enviados.html', {'mensajes': mensajes})
 
 def eliminar_mensajes(request):
-    # Obtener el usuario predeterminado
     usuario_predeterminado = User.objects.get(username='predeterminado')
 
     if request.method == 'POST':
-        # Obtener los IDs de los mensajes seleccionados
         mensajes_ids = request.POST.getlist('mensajes_ids')
         mensajes = Mensaje.objects.filter(pk__in=mensajes_ids, remitente=usuario_predeterminado)
         
-        # Eliminar los mensajes seleccionados
         mensajes.delete()
-        return redirect('ver_mensajes_enviados')  # Redirigir después de la eliminación
+        return redirect('ver_mensajes_enviados')  
 
-    # Mostrar la página con los mensajes
     mensajes = Mensaje.objects.filter(remitente=usuario_predeterminado)
     return render(request, 'mensajes/eliminar_mensajes.html', {'mensajes': mensajes})
 
